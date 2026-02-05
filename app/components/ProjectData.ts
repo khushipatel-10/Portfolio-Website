28ad0a2206c93f6706060bb83de49d9cf0530125
Khushi Patel <happipatel10@gmail.com>
2026-02-04 19:05:31 -0600

    Project date issue fix

--- app/components/ProjectData.ts
+++ app/components/ProjectData.ts
@@ -2,26 +2,31 @@ export const projects = [
   {
     slug: 'figma-to-html',
     title: 'Figma to HTML Converter',
+    date: 'Nov 2025',
     thumbnail: '/thumbnails/figma-2-html.png'
   },
   {
     slug: 'sous-duckling',
     title: 'Sous Duckling',
+    date: 'Aug 2025 - Oct 2025',
     thumbnail: '/thumbnails/sous-duckling.png'
   },
   {
     slug: 'omni-ai',
     title: 'Omni AI',
+    date: 'Jun 2025 - Jul 2025',
     thumbnail: '/thumbnails/omni.png'
   },
   {
     slug: 'insider-trading',
     title: 'Insider Trading Detection',
+    date: 'Jan 2025 - Apr 2025',
     thumbnail: '/thumbnails/trading.png'
   },
   {
     slug: 'nutrinet',
     title: 'NutriNet',
+    date: 'Jan 2025 - Apr 2025',
     thumbnail: '/thumbnails/nutri.png'
   }
 ];
